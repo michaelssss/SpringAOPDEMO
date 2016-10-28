d7ab91aaf0b8bbcbdc662c4e767b27cf1a946783
Michaelssss <liangyuming2@gmail.com>
2016-10-28 09:03:50 +0800

    有些坑不知道，现在知道了

--- src/main/java/com/Caller/Caller.java
+++ src/main/java/com/Caller/Caller.java
@@ -1,10 +1,4 @@
-/**
- * 文 件 名:  Caller.java
- * 版    权:  Huawei Technologies Co., Ltd. Copyright 2009-2011,  All rights reserved
- * 描    述:  <描述>
- * 创 建 人:  lWX364284
- * 创建时间:  2016-10-27
- */
+
 package com.Caller;
 
 import com.Funcs;
@@ -13,14 +7,6 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
-/**
- * <一句话功能简述>
- * <功能详细描述>
- *
- * @author lWX364284
- * @version [华为终端云统一账号模块, 2016-10-27]
- * @see [相关类/方法]
- */
 @Component
 public class Caller
 {
@@ -33,6 +19,8 @@ public class Caller
 
         Method method = funcs.getClass().getMethod("C");
         method.invoke(funcs);
+        //this will throw a Error 
+        
         Method B = funcs.getClass().getMethod("B");
         B.setAccessible(true);
         B.invoke(funcs);
